103db569cd863900fe1d40e8e67bc470f3234f32
Aarjit <poudelaarjit@gmail.com>
2023-06-07 21:02:27 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import "./App.css";
+import { Resizable } from "re-resizable";
+import Draggable from "react-draggable";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null,
+      rotation: 0,
+    };
+  }
+
+  handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      this.setState({
+        image: reader.result,
+      });
+    };
+    reader.readAsDataURL(file);
+  };
+
+  handleRotationChange = (event) => {
+    this.setState({
+      rotation: parseInt(event.target.value),
+    });
+  };
+
+  render() {
+    const { image, rotation } = this.state;
+    return (
+      <div>
+        <input type="file" accept="image/*" onChange={this.handleImageUpload} />
+        <input
+          type="range"
+          min="0"
+          max="360"
+          step="1"
+          value={rotation}
+          onChange={this.handleRotationChange}
+        />
+        <Draggable>
+          <Resizable
+            defaultSize={{
+              width: 200,
+              height: 360,
+            }}
+            style={{
+              position: "relative",
+              background: image ? `url(${image})` : "",
+              backgroundSize: "contain",
+              backgroundRepeat: "no-repeat",
+              transform: `rotate(${rotation}deg)`,
+            }}
+            lockAspectRatio={true}
+          >
+            <div></div>
+          </Resizable>
+        </Draggable>
+      </div>
+    );
+  }
 }
 
-export default App;
+render(<App />, document.querySelector("#root"));
